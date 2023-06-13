1c5a7168bb1b536fcdad95670e3c10ca3da4c858
BlakeJohnCarter <blakejohncarter@gmail.com>
2023-06-14 08:03:02 +1200

    Implement initial tests in Gilded Rose spec file
    Added initial test cases to the Gilded Rose spec file (test/jest/gilded-rose.spec.ts) to follow (TDD) approach.
    
    - The tests cover various scenarios and expected behavior of the Gilded Rose functionality.
    - This commit focuses on writing the test cases before implementing the corresponding functionality in the codebase.
    - The purpose is to ensure that the tests fail initially and can be used as a benchmark for future code changes and refactoring.
    - The implementation of the Gilded Rose class is yet to be done; this commit only includes the test cases.
    
    Intended new methods:
    
    - Updated the updateQuality method in the GildedRose class to introduce new methods for improved readability and maintainability.
    - The new methods include isAgedBrie, isBackstagePass, and isSulfuras to check the item type.
    - Implemented decreaseQuality, increaseQuality, and decreaseSellIn methods for specific item operations.

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,9 +1,69 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+  it('should decrease quality of normal item by 1', () => {
+    const gildedRose = new GildedRose([new Item('Normal Item', 5, 10)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+    expect(items[0].quality).toBe(9);
+  });
+
+  it('should decrease sellIn of normal item by 1', () => {
+    const gildedRose = new GildedRose([new Item('Normal Item', 5, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(4);
+  });
+
+  it('should increase quality of Aged Brie by 1', () => {
+    const gildedRose = new GildedRose([new Item('Aged Brie', 5, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(11);
+  });
+
+  it('should not increase quality of Aged Brie beyond 50', () => {
+    const gildedRose = new GildedRose([new Item('Aged Brie', 5, 50)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(50);
+  });
+
+  it('should decrease sellIn of Aged Brie by 1', () => {
+    const gildedRose = new GildedRose([new Item('Aged Brie', 5, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(4);
+  });
+
+  it('should increase quality of Backstage Passes by 1', () => {
+    const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(21);
+  });
+
+  it('should increase quality of Backstage Passes by 2 when sellIn is less than or equal to 10', () => {
+    const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 10, 20)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(22);
+  });
+
+  it('should increase quality of Backstage Passes by 3 when sellIn is less than or equal to 5', () => {
+    const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 5, 20)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(23);
+  });
+
+  it('should set quality of Backstage Passes to 0 when sellIn is less than 0', () => {
+    const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 0, 20)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+  });
+
+  it('should keep quality of Sulfuras unchanged', () => {
+    const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 10, 80)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(80);
+  });
+
+  it('should keep sellIn of Sulfuras unchanged', () => {
+    const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 10, 80)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(10);
   });
 });
